fda039d0a19c556626f137ff5913feb01bf3f175
Beth Tenorio <betsky98@gmail.com>
2018-11-07 22:45:55 -0500

    tune xgboost params for otto data

--- otto/otto-xgb-1.py
+++ otto/otto-xgb-1.py
@@ -9,6 +9,8 @@ https://www.kaggle.com/c/otto-group-product-classification-challenge/data
 Algorithms Used: XGB
 Submissions and Public Score:
 1-XGB - 0.47791
+2-XGB+params+n_estimators=250 - 0.64972
+3-XGB1+n_estimators=250 - 0.47791
 
 
 Reference:
@@ -44,3 +46,16 @@ pred = gbm.predict(test_xgb)
 submission = pd.DataFrame(pred, index=sample.id.values, columns=sample.columns[1:])
 submission.to_csv('xgb-1.csv', index_label='id')
 
+# Model 2: XGB with n_estimator=250 and params from otto-xgb-gridsearch.py
+params2 = {'base_score': 0.5, 'booster': 'gbtree', 'colsample_bylevel': 1, 'colsample_bytree': 1, 'gamma': 0, 'learning_rate': 0.1, 'max_delta_step': 0, 'max_depth': 3, 'min_child_weight': 1, 'n_estimators': 250, 'n_jobs': 1, 'objective': 'multi:softprob', 'random_state': 0, 'reg_alpha': 0, 'reg_lambda': 1, 'scale_pos_weight': 1, 'seed': 42, 'silent': True, 'subsample': 1, 'num_class': 9}
+gbm2 = xgb.train(params2, train_xgb, training_rounds)
+pred2 = gbm2.predict(test_xgb)
+submission2 = pd.DataFrame(pred2, index=sample.id.values, columns=sample.columns[1:])
+submission2.to_csv('xgb-2.csv', index_label='id')
+
+# Model 3: XGB with n_estimator=250 and default params
+params3 = {"objective": "multi:softprob", "eval_metric":"mlogloss", "num_class": 9, 'n_estimator': 250}
+gbm3 = xgb.train(params3, train_xgb, training_rounds)
+pred3 = gbm3.predict(test_xgb)
+submission3 = pd.DataFrame(pred3, index=sample.id.values, columns=sample.columns[1:])
+submission3.to_csv('xgb-3.csv', index_label='id')
