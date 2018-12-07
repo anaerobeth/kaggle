abdabdd0e78dcbc6d5eccaab1ce482f811eff644
Beth Tenorio <betsky98@gmail.com>
2018-12-06 21:32:17 -0500

    refactor FeatureTagger

--- ner/feature_tagger.py
+++ ner/feature_tagger.py
@@ -7,46 +7,47 @@ class FeatureTransformer(BaseEstimator, TransformerMixin):
         self.memory_tagger = MemoryTagger()
         self.tag_encoder = LabelEncoder()
         self.pos_encoder = LabelEncoder()
-        
+
     def fit(self, X, y):
-        words = X["Word"].values.tolist()
-        self.pos = X["POS"].values.tolist()
-        tags = X["Tag"].values.tolist()
+        words, self.pos, tags = map(lambda col: X[col].values.tolist(), ["Word", "POS", "Tag"])
         self.memory_tagger.fit(words, tags)
         self.tag_encoder.fit(tags)
         self.pos_encoder.fit(self.pos)
         return self
-    
+
+    def pos_default(self, p):
+        return self.pos_encoder.transform([p])[0] if p in self.pos else -1
+
+    def transform_tag(self, tag):
+        return self.tag_encoder.transform(tag)[0]
+
+    def set_defaults():
+        w = transform_tag['O'])
+        pos = pos_default(".")
+        return w, pos
+
+    def predict_tag(self, word):
+        return self.memory_tagger.predict(word)
+
     def transform(self, X, y=None):
-        def pos_default(p):
-            if p in self.pos:
-                return self.pos_encoder.transform([p])[0]
-            else:
-                return -1
-        
-        pos = X["POS"].values.tolist()
-        words = X["Word"].values.tolist()
+        pos, words = map(lambda col: X[col].values.tolist(), ["POS", "Word"])
         out = []
         for i in range(len(words)):
             w = words[i]
             p = pos[i]
             if i < len(words) - 1:
-                wp = self.tag_encoder.transform(self.memory_tagger.predict([words[i+1]]))[0]
+                wp = transform_tag(predict_tag([words[i+1]]))
                 posp = pos_default(pos[i+1])
             else:
-                wp = self.tag_encoder.transform(['O'])[0]
-                posp = pos_default(".")
+                wp, posp = set_defaults()
             if i > 0:
                 if words[i-1] != ".":
-                    wm = self.tag_encoder.transform(self.memory_tagger.predict([words[i-1]]))[0]
+                    wm = transform_tag(predict_tag([words[i-1]]))
                     posm = pos_default(pos[i-1])
                 else:
-                    wm = self.tag_encoder.transform(['O'])[0]
-                    posm = pos_default(".")
+                    wm, posm = set_defaults()
             else:
-                posm = pos_default(".")
-                wm = self.tag_encoder.transform(['O'])[0]
+                wm, posm = set_defaults()
             out.append(np.array([w.istitle(), w.islower(), w.isupper(), len(w), w.isdigit(), w.isalpha(),
-                                 self.tag_encoder.transform(self.memory_tagger.predict([w]))[0],
-                                 pos_default(p), wp, wm, posp, posm]))
+                                 transform_tag(predict_tag([w])), pos_default(p), wp, wm, posp, posm]))
         return out
